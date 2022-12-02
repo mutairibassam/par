99e7a7f9cf9d51af180c2524a3d0c363d0afc332
BassamMutairi <mutairibassam@gmail.com>
2022-12-02 14:26:45 +0300

    add bio & interest

--- schema/user.js
+++ schema/user.js
@@ -4,6 +4,7 @@ const userProfile = {
     uuid: "93499849382929838", // mandatory & auto-generated
     first_name: "Bassam", // mandatory
     last_name: "mutairi", // optional
+    bio: "Self-sufficient and reliable professional with 6 years of experience in software development. Successfully led various teams to build technological products, focusing on mobile and web applications.", // optional
     username: "mutairibassam", // mandatory
     email: "mutairibassam@gmail.com", // mandatory
     mobile: "0511111111", // optional
@@ -22,11 +23,12 @@ const userPost = {
     _id: "s9d8f79s", // mandatory & auto-generated
     username: "ref:username -> mutairibassam", // mandatory
     location: "ref:shopId -> 1", // mandatory
+    interest: ["nodejs", "ci/cd", "python"], // mandatory
     date: "19/03/2039", // mandatory
     from: "09:00 PM", // mandatory
     to: "10:00 PM", // mandatory
     slots: "3", // mandatory
-    occupied: "0", // mandatory & default to 0
+    occupied: "0", // mandatory & default to 0 & can't be more than slots
     status: "1", // mandatory -> [1 open, 2 deleted, 3 expired]
     createdAt: "19/02/2039", // mandatory & auto-generated,
     updatedAt: "20/02/2039", // mandatory & auto-generated,
